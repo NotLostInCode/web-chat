3972df8736a2c9f09382c18048ae4d9f7a16991c
Gregory <not.lost.in.code@gmail.com>
2023-06-05 21:48:30 +0400

    refactoring, assignment id

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,58 @@
-const socket = io()
+const socket = io();
 const sendMessageButton = document.querySelector(".message-send");
-const wrapperMessage = document.querySelector(".incoming-message");
+const wrapperMessage = document.querySelector(".wrapper-message");
 const messageInput = document.querySelector(".message-input");
+const body = document.querySelector("body");
 
+// Объявляем глобальные переменные
+let currentUserId; // Идентификатор текущего пользователя
+let senderId; // Идентификатор отправителя сообщения
 
-// Отправляем на backend
+// Отправляем сообщение на сервер
 function dataGeneration() {
-    if(messageInput.value) {
-        socket.emit('chat message', {
-            message: messageInput.value
-        })
-        messageInput.value = "";
-    }
+  if (messageInput.value) {
+    socket.emit("chat message", {
+      message: messageInput.value,
+      userId: currentUserId, // Передаем идентификатор пользователя
+    });
+    messageInput.value = "";
+  }
 }
-
-//
-sendMessageButton.addEventListener('click', dataGeneration)
-messageInput.addEventListener('keydown',(event) => event.code === "Enter" ?  dataGeneration() : '')
 //
 
-
-// Получаем id с сервера
-let currentUserId;
-
-socket.on('userId', (userId) => {
-    currentUserId = userId;
-  });
+// Получаем идентификатор пользователя с сервера
+socket.on("userId", (userId) => {
+  currentUserId = userId; // Сохраняем идентификатор пользователя
+});
 //
 
+// Принимаем сообщения с сервера
+socket.on("chat message", (data) => {
+  body.dataset.id = currentUserId; // Присваиваем идентификатор пользователя атрибуту `data-id` в теге body
+  // console.log(body.dataset.id);
 
-// Принимает данные с backend
-socket.on('chat message', (data) => {
-    console.log(currentUserId)
-    const message = document.createElement("div");
+  const message = document.createElement("div");
+  message.classList.add("message");
+  const messageText = document.createElement("p");
+  messageText.classList.add("message-text");
+  messageText.textContent = data.message;
 
-    message.classList.add("message");
-    message.textContent = data.message;
-    wrapperMessage.appendChild(message);
-})
+  senderId = data.sender; // Сохраняем идентификатор отправителя
+
+  if (data.sender === currentUserId) {
+    message.classList.add("outgoing-message");
+  } else {
+    message.classList.add("incoming-message");
+  }
+
+  wrapperMessage.appendChild(message);
+  message.appendChild(messageText);
+});
 //
 
+//
+sendMessageButton.addEventListener("click", dataGeneration);
+messageInput.addEventListener("keydown", (event) =>
+  event.code === "Enter" ? dataGeneration() : ""
+);
+//
--- index.js
+++ index.js
@@ -3,38 +3,30 @@ const express = require("express");
 //Подключаем библиотеку для генерации id
 const { v4: uuidv4 } = require("uuid");
 
-
-
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
 
 // Показываем по умолчанию, откуда нам брать статические файлы
 app.use(express.static(__dirname + "/assets/js"));
 app.use(express.static(__dirname + "/assets/css"));
 app.use(express.static(__dirname + "/assets/img"));
 
-
-// Принимаем сообщение
 io.on("connection", (socket) => {
-
-  // Отправляем id на фронтенд
+  // Генерируем и отправляем id на клиентскую сторону
   const userId = uuidv4();
   socket.emit("userId", userId);
 
-
-  // Прослушиваем событие, которое передаем с фронтенда
+  // Прослушиваем событие "chat message", переданное с клиентской стороны
   socket.on("chat message", (data) => {
-    // Обрабатываем и отправляем на фронтенд
+    // Обрабатываем сообщение и отправляем его на клиентскую сторону
     io.emit("chat message", {
       message: data.message,
-      sender: data.userId
+      sender: data.userId, // Используем переданный идентификатор пользователя
     });
   });
 });
